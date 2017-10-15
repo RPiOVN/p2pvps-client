9c5a51679cf52cdffe49514612a3d2ce4546de0f
Chris Troutner <chris.troutner@gmail.com>
2017-10-15 23:08:23 +0000

    Updating gitignore

--- client/simple/reverse-tunnel-generated.js
+++ client/simple/reverse-tunnel-generated.js
@@ -1,11 +1,11 @@
 var tunnel = require('reverse-tunnel-ssh');
 tunnel({
-  host: '174.138.35.118',
+  host: '192.168.2.2',
   port: 6100,
   username: 'sshuser',
   password: 'sshuserpassword',
   dstHost: '0.0.0.0',
-  dstPort: 6018, 
+  dstPort: 6001, 
   srcPort: 3100 
 }, function(error, clientConnection) {
   if(error)
