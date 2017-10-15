e6d67c7f4d2b13a4e9c04d38922995f6844dc703
Chris Troutner <chris.troutner@gmail.com>
2017-10-15 23:42:41 +0000

    changing executable permissions for script files

--- client/simple/reverse-tunnel-generated.js
+++ client/simple/reverse-tunnel-generated.js
@@ -1,11 +1,11 @@
 var tunnel = require('reverse-tunnel-ssh');
 tunnel({
-  host: '192.168.2.2',
+  host: '174.138.35.118',
   port: 6100,
   username: 'sshuser',
   password: 'sshuserpassword',
   dstHost: '0.0.0.0',
-  dstPort: 6001, 
+  dstPort: 6002, 
   srcPort: 3100 
 }, function(error, clientConnection) {
   if(error)
