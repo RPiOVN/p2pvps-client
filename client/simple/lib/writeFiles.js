61a505006a760c5e7cbb8a4f4e5db6bb09520277
Chris Troutner <chris.troutner@gmail.com>
2017-11-27 07:54:58 -0800

    Fixing from file changes

--- client/simple/lib/writeFiles.js
+++ client/simple/lib/writeFiles.js
@@ -22,54 +22,57 @@ function Constructor() {
   // This function writes out the Dockerfile.
   this.writeDockerfile = function(port, username, password) {
 
-    var promise = new Promise.Promise();
-    
-    var fileString = "FROM resin/rpi-raspbian\n"+
-        "MAINTAINER Chris Troutner <chris.troutner@gmail.com>\n"+
-        "RUN apt-get update\n"+
-        "RUN apt-get install -y openssh-server\n"+
-        "RUN apt-get install nano\n"+
-        "RUN apt-get install ssh\n"+
-        "RUN mkdir /var/run/sshd\n"+
-        "RUN sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd\n"+
-        "ENV NOTVISIBLE \"in users profile\"\n"+
-        "RUN echo \"export VISIBLE=now\" >> /etc/profile\n"+
-        "RUN curl -sL https://deb.nodesource.com/setup_4.x -o nodesource_setup.sh\n"+
-        "RUN bash nodesource_setup.sh\n"+
-        "RUN apt-get install -y nodejs\n"+
-        "RUN apt-get install -y build-essential\n"+
-        "WORKDIR /root\n"+
-        "COPY package.json package.json\n"+
-        "RUN npm install\n"+
-        "EXPOSE 3100\n"+
-        "COPY dummyapp.js dummyapp.js\n"+
-        "COPY finalsetup finalsetup\n"+
-        "COPY connectClient.js connectClient.js\n"+
-        "COPY package.json package.json\n"+
-        "COPY config.json config.json\n"+
-        "COPY reverse-tunnel-generated.js reverse-tunnel.js\n"+
-        "RUN chmod 775 finalsetup\n"+
-        "RUN useradd -ms /bin/bash "+username+"\n"+
-        "RUN echo "+username+":"+password+" | chpasswd\n"+
-        "EXPOSE "+port+"\n"+
-        //"ENTRYPOINT [\"./finalsetup\", \"node\", \"dummyapp.js\"]\n";
-        "ENTRYPOINT [\"./finalsetup\", \"node\", \"connectClient.js\"]\n";
+    var promise = new Promise(function(resolve, reject) {
+
+      var fileString = "FROM resin/rpi-raspbian\n"+
+          "MAINTAINER Chris Troutner <chris.troutner@gmail.com>\n"+
+          "RUN apt-get update\n"+
+          "RUN apt-get install -y openssh-server\n"+
+          "RUN apt-get install nano\n"+
+          "RUN apt-get install ssh\n"+
+          "RUN mkdir /var/run/sshd\n"+
+          "RUN sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd\n"+
+          "ENV NOTVISIBLE \"in users profile\"\n"+
+          "RUN echo \"export VISIBLE=now\" >> /etc/profile\n"+
+          "RUN curl -sL https://deb.nodesource.com/setup_4.x -o nodesource_setup.sh\n"+
+          "RUN bash nodesource_setup.sh\n"+
+          "RUN apt-get install -y nodejs\n"+
+          "RUN apt-get install -y build-essential\n"+
+          "WORKDIR /root\n"+
+          "COPY package.json package.json\n"+
+          "RUN npm install\n"+
+          "EXPOSE 3100\n"+
+          "COPY dummyapp.js dummyapp.js\n"+
+          "COPY finalsetup finalsetup\n"+
+          "COPY connectClient.js connectClient.js\n"+
+          "COPY package.json package.json\n"+
+          "COPY config.json config.json\n"+
+          "COPY reverse-tunnel-generated.js reverse-tunnel.js\n"+
+          "RUN chmod 775 finalsetup\n"+
+          "RUN useradd -ms /bin/bash "+username+"\n"+
+          "RUN echo "+username+":"+password+" | chpasswd\n"+
+          "EXPOSE "+port+"\n"+
+          //"ENTRYPOINT [\"./finalsetup\", \"node\", \"dummyapp.js\"]\n";
+          "ENTRYPOINT [\"./finalsetup\", \"node\", \"connectClient.js\"]\n";
 
 
-    fs.writeFile('./support_files/Dockerfile', fileString, function (err) {
+      fs.writeFile('./support_files/Dockerfile', fileString, function (err) {
 
-      if(err) {
-        debugger;
-        console.error('Error while trying to write file: ', err);
-        promise.reject(err);
+        if(err) {
+          debugger;
+          console.error('Error while trying to write file: ', err);
+          reject(err);
 
-      } else {
-        console.log('Dockerfile written successfully!');
-        promise.resolve();
-      }
-    });
-    
+        } else {
+          console.log('Dockerfile written successfully!');
+          resolve();
+        }
+      });
+
+    }
+      
     return promise;
+    
   };
 
   // Write out the reverse-tunnel-generated.js file. This was used in an older prototype before
@@ -77,35 +80,35 @@ function Constructor() {
   this.writeReverseTunnel = function(port, username, password) {
     debugger;
     
-    var promise = new Promise.Promise();
-    
-    var fileString = "var tunnel = require('reverse-tunnel-ssh');\n"+
-      "tunnel({\n"+
-      "  host: '"+global.sshServerIp+"',\n"+
-      "  port: "+global.sshServerPort+",\n"+
-      "  username: 'sshuser',\n"+
-      "  password: 'sshuserpassword',\n"+
-      "  dstHost: '0.0.0.0',\n"+
-      "  dstPort: "+port+", \n"+
-      "  srcPort: 3100 \n"+
-      "}, function(error, clientConnection) {\n"+
-      "  if(error)\n"+
-      "    console.error('Error! ', error);\n"+
-      "});";
-    
-    fs.writeFile('./support_files/reverse-tunnel-generated.js', fileString, function (err) {
+    var promise = new Promise(function(resolve, reject) {
 
-      if(err) {
-        debugger;
-        console.error('Error while trying to write file: ', err);
-        promise.reject(err);
-        
-      } else {
-        console.log('reverse-tunnel-generated.js written successfully!');
-        promise.resolve();
-      }
-    });
-    
+      var fileString = "var tunnel = require('reverse-tunnel-ssh');\n"+
+        "tunnel({\n"+
+        "  host: '"+global.sshServerIp+"',\n"+
+        "  port: "+global.sshServerPort+",\n"+
+        "  username: 'sshuser',\n"+
+        "  password: 'sshuserpassword',\n"+
+        "  dstHost: '0.0.0.0',\n"+
+        "  dstPort: "+port+", \n"+
+        "  srcPort: 3100 \n"+
+        "}, function(error, clientConnection) {\n"+
+        "  if(error)\n"+
+        "    console.error('Error! ', error);\n"+
+        "});";
+
+      fs.writeFile('./support_files/reverse-tunnel-generated.js', fileString, function (err) {
+
+        if(err) {
+          debugger;
+          console.error('Error while trying to write file: ', err);
+          reject(err);
+
+        } else {
+          console.log('reverse-tunnel-generated.js written successfully!');
+          resolve();
+        }
+      });
+    }
     return promise;
   };
   
@@ -114,32 +117,32 @@ function Constructor() {
   this.writeClientConfig = function(port, deviceId) {
     debugger;
     
-    var promise = new Promise.Promise();
-    
-    var fileString = "{\n"+
-      '"deviceId": "'+deviceId+'",\n'+
-      //'"serverIp": "192.241.214.57",\n'+
-      '"serverIp": "p2pvps.net",\n'+
-      '"serverPort": "3001",\n'+
-      //'"sshServer": "174.138.35.118",\n'+
-      '"sshServer": "p2pvps.net",\n'+
-      '"sshServerPort": 6100,\n'+
-      '"sshTunnelPort": '+port+'\n'+
-      '}\n';
+    var promise = new Promise(function(resolve, reject) {
 
-    fs.writeFile('./support_files/config.json', fileString, function (err) {
+      var fileString = "{\n"+
+        '"deviceId": "'+deviceId+'",\n'+
+        //'"serverIp": "192.241.214.57",\n'+
+        '"serverIp": "p2pvps.net",\n'+
+        '"serverPort": "3001",\n'+
+        //'"sshServer": "174.138.35.118",\n'+
+        '"sshServer": "p2pvps.net",\n'+
+        '"sshServerPort": 6100,\n'+
+        '"sshTunnelPort": '+port+'\n'+
+        '}\n';
 
-      if(err) {
-        debugger;
-        console.error('Error while trying to write config.json file: ', err);
-        promise.reject(err);
-        
-      } else {
-        console.log('config.json written successfully!');
-        promise.resolve();
-      }
-    });
-    
+      fs.writeFile('./support_files/config.json', fileString, function (err) {
+
+        if(err) {
+          debugger;
+          console.error('Error while trying to write config.json file: ', err);
+          reject(err);
+
+        } else {
+          console.log('config.json written successfully!');
+          resolve();
+        }
+      });
+    }
     return promise;
   };
 
